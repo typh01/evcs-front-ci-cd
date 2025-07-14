75962f0e979b53ff840ede3546a1009f1200495d
typh01 <127132180+typh01@users.noreply.github.com>
2025-07-14 11:37:11 +0900

    2025-07-14 일부 수정

--- src/components/UserInterface/Board/Event/EventBoard.jsx
+++ src/components/UserInterface/Board/Event/EventBoard.jsx
@@ -136,22 +136,22 @@ const EventBoard = () => {
                           </tr>
                         </thead>
                         <tbody>
-                          {events.map((event) => (
-                            <tr
-                              key={event.eventNo}
-                              style={{ cursor: "pointer" }}
-                              onClick={() =>
-                                navigate("/goEventDetailPage", {
-                                  state: { event: event }, // ← 여기서 객체 넘기기
-                                })
-                              }
-                            >
-                              <td>{event.eventNo}</td>
-                              <td>{event.eventName}</td>
-                              <td>{event.memberNickname}</td>
-                              <td>{event.enrollDate}</td>
-                            </tr>
-                          ))}
+                          {Array.isArray(events) &&
+                            events.map((event) => (
+                              <tr
+                                key={event.eventNo}
+                                onClick={() =>
+                                  navigate("/goEventDetailPage", {
+                                    state: { event },
+                                  })
+                                }
+                              >
+                                <td>{event.eventNo}</td>
+                                <td>{event.eventName}</td>
+                                <td>{event.memberNickname}</td>
+                                <td>{event.enrollDate}</td>
+                              </tr>
+                            ))}
                         </tbody>
                       </Table>
                     </Card.Body>
